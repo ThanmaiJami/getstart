b50b4158ef61be492270d20067a499b1474b46ab
Jami Sri Valli Lakshmi Thanmai <70189537+ThanmaiJami@users.noreply.github.com>
2023-08-30 12:14:19 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-
-principal=1000
-rate=0.05
-time=2
-
-interest=$((principal * rate * time))
-
-echo "Simple Interest: $interest"
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+   # Do not use this in production. Sample purpose only.
+   # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # <ThanmaiJami>
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
+   # Output:
+   # simple interest = p*t*r
+   echo "Enter the principal:"
+   read p
+   echo "Enter rate of interest per year:"
+   read r
+   echo "Enter time period in years:"
+   read t
+   s=`expr $p \* $t \* $r / 100`
+   echo "The simple interest is: "
+   echo $s
